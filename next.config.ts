f084da575211d0b82568352aeed6fbacbc86a161
zubi-afridi <zubairafridi2312@gmail.com>
2026-03-09 11:27:10 +0500

    fix vercel build

--- next.config.ts
+++ next.config.ts
@@ -4,28 +4,24 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
 
   webpack(config) {
-    // 1. Find the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule: any) =>
       rule.test?.test?.(".svg"),
     );
 
     config.module.rules.push(
-      // 2. Re-apply the existing rule, but only for svg imports ending in ?url
       {
         ...fileLoaderRule,
         test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
+        resourceQuery: /url/,
       },
-      // 3. Convert all other *.svg imports to React components
       {
         test: /\.svg$/i,
         issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
         use: ["@svgr/webpack"],
       },
     );
 
-    // 4. Modify the file loader rule to ignore *.svg
     fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
