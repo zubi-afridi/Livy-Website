38abc54105351b9e213c01ef095234aedd776e3d
zubi-afridi <zubairafridi2312@gmail.com>
2026-03-17 14:06:45 +0500

    Fixed UI of Auth Pages

--- src/components/auth/AuthShell.tsx
+++ src/components/auth/AuthShell.tsx
@@ -7,13 +7,13 @@ export default function AuthShell({ children }: { children: React.ReactNode }) {
       <div className="xl:h-6 lg:h-4 h-3 w-full bg-primary hidden sm:block fixed z-10" />
 
       <main className="flex-1 flex items-center justify-center px-4 sm:py-10 py-5">
-        <div className="w-full max-w-105 flex flex-col items-center sm:gap-6 gap-2">
+        <div className="w-full max-w-115 flex flex-col items-center sm:gap-6 gap-2">
           <Image
             src="/icons/logo.svg"
             alt="Livy"
             width={120}
             height={60}
-            className="h-auto w-26 lg:w-32 xl:w-39"
+            className="h-auto w-39"
             priority
             data-aos="fade-down"
             data-aos-duration="800"
