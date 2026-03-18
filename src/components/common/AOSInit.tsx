47bd1aac6793da8bd48850163c4e29d5284a02d1
zubi-afridi <zubairafridi2312@gmail.com>
2026-03-18 17:58:08 +0500

    Fixed the animation problem, animate once

--- src/components/common/AOSInit.tsx
+++ src/components/common/AOSInit.tsx
@@ -8,8 +8,8 @@ const AOSInit = () => {
     AOS.init({
       duration: 1000,
       easing: "ease-in-out",
-      once: false,
-      mirror: true,
+      once: true,
+      mirror: false,
       offset: 120,
       delay: 0,
       anchorPlacement: "top-bottom",
